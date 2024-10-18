64dc0748e54152a6e73b7337a4c704aceff2df02
Zebbb01 <geraldvillaceran01@gmail.com>
2024-10-18 20:21:54 +0800

    change or in strong password

--- backend/app.py
+++ backend/app.py
@@ -41,7 +41,7 @@ def is_strong_password(password):
     if (len(password) >= 8 and
             re.search(r"[A-Z]", password) and
             re.search(r"[a-z]", password) and
-            re.search(r"\d", password) and
+            re.search(r"\d", password) or
             re.search(r"[!@#$%^&*_()-,.?\":{}|<>]", password)):  # Include special characters
         return True
     return False
